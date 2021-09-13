02da0f7587a4ffe5714f187f7561b831012962c6
rovijun <rovijun@gmail.com>
2021-09-13 11:09:59 +0200

    exo00

---
+++ Day00/exo00/hitmachine2000.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+   std::cout << "Moi je m'appelle 'Lolita'\nLo ou bien Lola, du pareil au même\nMoi je m'appelle 'Lolita'\nQuand je rêve aux loups, c'est Lola qui saigne\n[...]\nC'est pas ma faute\nEt quand je donne ma langue aux chats je vois les autres\nTout prêts à se jeter sur moi, c'est pas ma faute à moi\nSi j'entends tout autour de moi\nL.O.L.I.T.A, moi 'Lolita'" << std::endl; 
+   
+   return 0;
+}
