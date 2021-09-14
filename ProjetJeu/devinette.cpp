353c60cff5279a20fc30ed30cee5e6371caa2e20
rovijun <rovijun@gmail.com>
2021-09-14 15:21:30 +0200

    update

--- ProjetJeu/devinette.cpp
+++ ProjetJeu/devinette.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <ctime>
 
-
-
-
 int main() {
 
     int number;
@@ -12,7 +9,11 @@ int main() {
     srand(time(0));
     int secret = rand()%1000 + 1;
 
-    // std::cout << secret;
+    std::string pseudo;
+
+    std::cout << "Quel est ton pseudo ? ";
+    std::cin >> pseudo;
+    std::cout << "Bonjour " << pseudo << ", la partie commence!\n";
 
     std::cout << "Devine le numéro secret entre 1 et 1000 : ";
     std::cin >> number;
@@ -35,8 +36,8 @@ int main() {
 
     } 
 
-    std::cout << "Bravo!";
-    std::cout << "Tu as trouve au bout de " << tentative << " tentatives.\n";
+    std::cout << "Bravo " << pseudo << " !\n";
+    std::cout << "Tu as trouve au bout de " << tentative << " tentatives!\n";
 
 
     return 0;
