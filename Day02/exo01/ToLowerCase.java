9b00be34e00e0ee65d445effa5a56f05adb5622c
rovijun <rovijun@gmail.com>
2021-09-15 14:14:23 +0200

    Day02 exo01

---
+++ Day02/exo01/ToLowerCase.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Main {
+
+   public static void main(String args[]) {
+      String Str = new String("SALUT MON AMI LUDOVIC T'ES VRAIMENT UN MEC GENIAL");
+
+      System.out.print("Return Value :");
+      System.out.println(Str.toLowerCase());
+   }
+}
